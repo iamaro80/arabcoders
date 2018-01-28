1f89fac3c9ee6b24d690d72e658f00f2e3aefe45
iamaro80 <ibrahimmaro@gmail.com>
2018-01-28 02:00:25 +0200

    biggest number with max function

--- biggist_number.py
+++ biggist_number.py
@@ -11,6 +11,11 @@ def bigger(a,b):
 def biggest(a,b,c):
 	return bigger(bigger(a,b),c)
 
+# the same behaivour using max 
+
+def max_number(a, b, c):
+	return max(a,b,c)
+
 
 # Test Cases
 
@@ -27,3 +32,6 @@ print biggest(3, 3, 9)
 #>>> 9
 
 print biggest(9, 3, 9)
+
+# Test Cases : max_number
+print max_number(15,2,3)

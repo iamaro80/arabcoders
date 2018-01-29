637f6a655102c763d744dee3d1639bf13672514c
iamaro80 <ibrahimmaro@gmail.com>
2018-01-29 16:27:43 +0200

    mutation and aliasing

--- Test_Drive.py
+++ Test_Drive.py
@@ -1,17 +1,5 @@
+spy = [0,0,7]
+agent = spy
+spy[2] = agent[2] + 1
 
-# We defined:
-
-stooges = ['Moe','Larry','Curly']
-
-# but in some Stooges films, Curly was
-# replaced by Shemp.
-
-# Write one line of code that changes
-# the value of stooges to be:
-
-['Moe','Larry','Shemp']
-
-# but does not create a new List
-# object.
-stooges[2] = 'Shemp'
-print stooges
+print agent[2]

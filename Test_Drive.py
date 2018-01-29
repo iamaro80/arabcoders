9696cf34dd3d52f91f195e0ab90a585b008319ea
iamaro80 <ibrahimmaro@gmail.com>
2018-01-29 15:21:15 +0200

    lesson 12

--- Test_Drive.py
+++ Test_Drive.py
@@ -1,1 +1,14 @@
-print 'Y'
+# We defined:
+
+stooges = ['Moe','Larry','Curly']
+
+# but in some Stooges films, Curly was
+# replaced by Shemp.
+
+# Write one line of code that changes
+# the value of stooges to be:
+
+['Moe','Larry','Shemp']
+
+# but does not create a new List
+# object.

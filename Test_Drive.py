bd5c4d7bf738d1b952996c4c67ff7020a3d4327d
iamaro80 <ibrahimmaro@gmail.com>
2018-01-30 13:28:52 +0200

    find_element ===> Udacity solution 3

--- Test_Drive.py
+++ Test_Drive.py
@@ -1,20 +1,21 @@
 # Define a procedure, find_element,
-# that takes as its inputs a list
-# and a value of any type, and
-# returns the index of the first
-# element in the input list that
-# matches the value.
+# using index that takes as its
+# inputs a list and a value of any
+# type, and returns the index of
+# the first element in the input
+# list that matches the value.
 
 # If there is no matching element,
 # return -1.
 
 def find_element(p,t):
-    i = 0
-    for e in p:
-        if e == t:
-            return i
-        i += 1
-    return -1
+    if t in p:
+        return p.index(t)
+    else:
+        return -1    
+
+
+
 
 
 print find_element([1,2,3],3)

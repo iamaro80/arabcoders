95ed65e36d44d340555569ec44ca0aa156f02188
iamaro80 <ibrahimmaro@gmail.com>
2018-01-30 12:33:59 +0200

    sum_list

--- Test_Drive.py
+++ Test_Drive.py
@@ -1,9 +1,20 @@
-def print_all_elements(p):
-    i = 0
-    while i < len(p):
-        print p[i]
-        i = i + 1
+# Define a procedure, sum_list,
+# that takes as its input a
+# list of numbers, and returns
+# the sum of all the elements in
+# the input list.
 
-p = [1,2,3,4]
 
-print_all_elements(p)
+def sum_list(alist):
+    sum =  0
+    for n in alist:
+        sum += n
+    return sum
+print sum_list([1, 7, 4])
+#>>> 12
+
+print sum_list([9, 4, 10])
+#>>> 23
+
+print sum_list([44, 14, 76])
+#>>> 134

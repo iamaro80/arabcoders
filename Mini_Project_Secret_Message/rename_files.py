fb73956db3f65fd712be109f564420f368e7d96f
iamaro80 <ibrahimmaro@gmail.com>
2018-02-06 17:16:04 +0200

    secret message project

--- Mini_Project_Secret_Message/rename_files.py
+++ Mini_Project_Secret_Message/rename_files.py
@@ -1,6 +1,18 @@
 # Mini-Project: Secret Message
 import os
 
-files_list= os.listdir('D://OMAC//Mini-Project_Secret_Message//prank')
+def rename_files():
+    file_list = os.listdir(r'C:\udacity\prank')
+    print file_list
+    saved_path = os.getcwd()
+    working_path = 'C:\udacity\prank'
+    os.chdir(working_path)
 
-print type(files_list)
+    for file_name in file_list:
+        print 'old name', file_name
+        print 'new name', file_name.translate(None,'0123456789')
+        os.rename(file_name, file_name.translate(None,'0123456789'))
+    os.chdir(saved_path)
+    print os.getcwd()
+
+rename_files()
--- Mini_Project_Secret_Message/secret.py
+++
@@ -1,2 +0,0 @@
-# Mini-Project: Secret Message
-import os

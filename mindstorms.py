cc2288877adb84cd2fc15d0b00828b35c8791524
Ibrahim Maro <ibrahimmaro@gmail.com>
2018-02-18 18:13:25 +0200

    draw square

---
+++ mindstorms.py
@@ -0,0 +1,22 @@
+import turtle
+import time
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor('#cebbbe')
+
+    brad = turtle.Turtle()
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(75)
+
+
+
+    window.exitonclick()
+
+draw_square()

7822633e362e739d36dcd2d5f81935063c49674b
iamaro80 <ibrahimmaro@gmail.com>
2018-02-20 21:47:29 +0200

    one step forward

--- check_profanity.py
+++ check_profanity.py
@@ -1,11 +1,27 @@
+import urllib
+
 def read_text():
     quote = open(r'C:\GitHub\arabcoders\movie_quotes.txt')
     content_of_file = quote.read()
-    print 'name of the file is:', quote.name
-    print content_of_file
     quote.close()
+    check_profanity(content_of_file)
+
+
+def check_profanity(text_to_check):
+    aut = urllib
+    connection = urllib.urlopen('http://www.wdylike.appspot.com/?q='+text_to_check)
+    output = connection.read()
+    if 'false' in output:
+        print 'Profanity Alert!!'
+    elif 'true' in output:
+        print 'This document has no curse word!'
+    else:
+        print 'Could not scan the document probably.'
+    connection.close()
+
 
 read_text()
+
 
 
 

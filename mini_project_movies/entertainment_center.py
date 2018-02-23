3b7efeb7a2e42d04b5f425845b25b385e6406f29
iamaro80 <ibrahimmaro@gmail.com>
2018-02-24 00:00:53 +0200

    Understand class

---
+++ mini_project_movies/entertainment_center.py
@@ -0,0 +1,10 @@
+import media
+
+toy_story = media.Movie('Toy Story',
+                        'A story of a boy and his toys that comes to life',
+                        'https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg',
+                        'https://youtu.be/KYz2wyBy3kc')
+
+print toy_story.storyline
+
+

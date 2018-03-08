dbaaf9f55b41dd910463a9e6aca39147d5a047b4
iamaro80 <ibrahimmaro@gmail.com>
2018-03-08 14:48:28 +0200

    mini project: Movies

--- mini_project_movies/entertainment_center.py
+++ mini_project_movies/entertainment_center.py
@@ -1,10 +1,20 @@
 import media
+import fresh_tomatoes
 
-toy_story = media.Movie('Toy Story',
-                        'A story of a boy and his toys that comes to life',
-                        'https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg',
-                        'https://youtu.be/KYz2wyBy3kc')
+terminal = media.Movie('Terminal',
+                       'A man stuck in airport',
+                       'https://goo.gl/X386JQ',
+                       'https://youtu.be/GZjC9dAvWuU?t=11')
 
-print toy_story.storyline
+the_man_who_knew_infinity = media.Movie('The Man Who Knew Infinity',
+                                        'The story of Srinivasa Ramanujan',
+                                        'https://goo.gl/1WyLNe',
+                                        'https://youtu.be/NP0lUqNAw3k')
+fault_in_our_stars = media.Movie('Fault in our stars',
+                                 'A story of teenagers fighting the cancer',
+                                 'https://goo.gl/KGganw',
+                                 'https://youtu.be/9ItBvH5J6ss')
 
+movies = [terminal, the_man_who_knew_infinity, fault_in_our_stars]
 
+fresh_tomatoes.open_movies_page(movies)
